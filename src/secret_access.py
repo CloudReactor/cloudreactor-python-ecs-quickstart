40dd45f00fd7f810ce687e0c56670abd2374d0a6
Jeff Tsay <jctsay@aim.com>
2021-01-26 11:08:36 -0800

    Test package 2.0.0

---
+++ src/secret_access.py
@@ -0,0 +1,28 @@
+#!/usr/local/bin/python
+
+import logging
+import os
+
+from dotenv import load_dotenv
+
+from proc_wrapper import StatusUpdater
+
+
+def main():
+    load_dotenv()
+
+    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s: %(message)s")
+    logger = logging.getLogger(__name__)
+
+    with StatusUpdater(logger=logger) as updater:
+        logging.info('Starting secret access ...')
+        secret_value = os.environ.get('SECRET_VALUE')
+
+        if secret_value:
+            updater.send_update(last_status_message=f'Got secret value: {secret_value}',
+                    success_count=len(secret_value))
+        else:
+            raise RuntimeError('Failed to read secret')
+
+if __name__ == '__main__':
+    main()
