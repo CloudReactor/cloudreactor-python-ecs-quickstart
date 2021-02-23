860d3de5fcbe759ac81b41e3939d262907833552
jtsay <jtsay@cloudreactor.io>
2021-02-23 13:37:22 -0800

    Fix bash script for copying requirements

--- copy_requirements.sh
+++ copy_requirements.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "Copying requirement files back to host ..."
-set IMAGE_NAME=cloudreactor-ecs-quickstart-dev
-set TEMP_CONTAINER_NAME="$IMAGE_NAME-temp"
+IMAGE_NAME=cloudreactor-ecs-quickstart-dev
+TEMP_CONTAINER_NAME="$IMAGE_NAME-temp"
 
 docker create --name $TEMP_CONTAINER_NAME $IMAGE_NAME
 docker cp $TEMP_CONTAINER_NAME:/tmp/requirements.txt requirements.txt
 docker cp $TEMP_CONTAINER_NAME:/tmp/dev-requirements.txt dev-requirements.txt
 docker rm $TEMP_CONTAINER_NAME
 
-ECHO Done copying requirement files back to host.
+echo "Done copying requirement files back to host."
