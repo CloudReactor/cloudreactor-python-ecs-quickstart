6d72b740a47f5d9aa49011d418f5506e4464056f
jtsay <jtsay@cloudreactor.io>
2022-06-01 19:49:32 -0700

    Build before copying requirements

--- copy_requirements.sh
+++ copy_requirements.sh
@@ -5,6 +5,7 @@ echo "Copying requirement files back to host ..."
 IMAGE_NAME=cloudreactor-python-ecs-quickstart-dev
 TEMP_CONTAINER_NAME="$IMAGE_NAME-temp"
 
+docker compose build pytest
 docker create --name $TEMP_CONTAINER_NAME $IMAGE_NAME
 docker cp $TEMP_CONTAINER_NAME:/tmp/requirements.txt requirements.txt
 docker cp $TEMP_CONTAINER_NAME:/tmp/dev-requirements.txt dev-requirements.txt
